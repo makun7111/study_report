3504a85395b5c2be479a84e983258e0f079d95cf
DESKTOP-SB45AQR\81802 <makun1010IT@gmail.com>
2022-09-04 12:15:23 +0900

    メールの送信するための変更

--- diary/views.py
+++ diary/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-
+import logging
 # Create your views here.
+from django.urls import reverse_lazy
 from django.views import generic
+from.forms import InquiryForm
 
 class IndexView(generic.TemplateView):
     template_name="index.html"
+
+logger=logging.getLogger(__name__)
+class InquiryView(generic.FormView):
+    template_name="inquiry.html"
+    form_class = InquiryForm
+    success_url = reverse_lazy('diary:inquiry')
+    def form_valid(self,form):
+        form.send_email()
+        logger.info('inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
+
+
+
