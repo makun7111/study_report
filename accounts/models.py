e7eb0baa0f75b55560ac26d145f30df6a17549ff
DESKTOP-SB45AQR\81802 <makun1010IT@gmail.com>
2022-09-04 19:02:40 +0900

    認証の初期設定

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
-    """拡張ユ－ザーモデル"""
-
     class Meta:
-        verbosa_name_plural='CustomeUser'
+        verbose_name_plural='CustomUser'
