05db903869376f4097ba5588d7adcfc0056f2cbf
DESKTOP-SB45AQR\81802 <makun1010IT@gmail.com>
2022-09-04 12:22:09 +0900

    プロジェクト設定ファイルを分割

---
+++ private_diary/settings_dev.py
@@ -0,0 +1,43 @@
+from .settings_common import*
+SECRET_KEY = 'django-insecure-bt(00h8k#9a3e%ya%1zu=n6$ljlqp8$w5@g7_ubo@agd0!-1vr'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+LOGGING={
+    'version':1,
+    'disable_existing_loggers':False,
+
+    'loggers':{
+        'django':{
+        'handlers':['console'],
+        'level':'INFO',
+        },
+        'diary':{
+            'handlers':['console'],
+            'level':'DEBUG',
+        },
+
+    },
+
+    'handlers':{
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter':'dev'
+        },
+    },
+
+    'formatters':{
+        'dev':{
+            'format':'\t'.join([
+                '%(asctime)s',
+                '[%(leveltime)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        },
+    }
+}
