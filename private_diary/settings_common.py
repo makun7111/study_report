6a8838a569f93951a62c933cf19ea74d87d9fd97
DESKTOP-SB45AQR\81802 <makun1010IT@gmail.com>
2022-09-14 18:28:57 +0900

    5

--- private_diary/settings_common.py
+++ private_diary/settings_common.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME':'private_diary',
-        'USER':'DB_USER',
-        'PASSWORD':'DB_PASSWORD',
+        'USER':os.environ.get('DB_USER'),
+        'PASSWORD':os.environ.get('DB_PASSWORD'),
         'HOST':'',
         'PORT':'',
     }
