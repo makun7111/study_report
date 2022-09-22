187d84e42646dbf24891c7d15467900ac6b4f377
DESKTOP-SB45AQR\81802 <makun1010IT@gmail.com>
2022-09-22 17:03:44 +0900

    motoni

--- diary/models.py
+++ diary/models.py
@@ -4,9 +4,10 @@ class Diary(models. Model):
 
     user = models.ForeignKey(CustomUser,verbose_name='ユーザー', on_delete=models.PROTECT)
     title = models.CharField(verbose_name='タイトル',max_length=40)
-    time = models.IntegerField(verbose_name='時間')
-    materials = models.CharField(verbose_name='教材',max_length=40)
-    comment = models.TextField(verbose_name='コメント',blank=True, null=True)
+    content = models.TextField(verbose_name='本文',blank=True, null=True)
+    photo1 = models.ImageField(verbose_name='写真　1',blank=True, null=True)
+    photo2 = models.ImageField(verbose_name='写真　2',blank=True, null=True)
+    photo3 = models.ImageField(verbose_name='写真　3',blank=True, null=True)
     created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='更新日時',auto_now=True)
 
