7a5391362f63fcde7bf61370bdaefd3be3722d13
DESKTOP-SB45AQR\81802 <makun1010IT@gmail.com>
2022-09-22 16:58:39 +0900

    Revert "Revert "デフォルト値の追加""
    This reverts commit cabe957e6a330d9944ae3c2e191148a40d41ae92.

--- diary/models.py
+++ diary/models.py
@@ -4,9 +4,9 @@ class Diary(models. Model):
 
     user = models.ForeignKey(CustomUser,verbose_name='ユーザー', on_delete=models.PROTECT)
     title = models.CharField(verbose_name='タイトル',max_length=40)
-    time = models.IntegerField(verbose_name='時間')
-    materials = models.CharField(verbose_name='教材',max_length=40)
-    comment = models.TextField(verbose_name='コメント',blank=True, null=True)
+    time = models.IntegerField(verbose_name='時間',default=0)
+    materials = models.CharField(verbose_name='教材',max_length=40,default='nothing')
+    comment = models.TextField(verbose_name='コメント',blank=True, null=True,default='nothing')
     created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='更新日時',auto_now=True)
 
