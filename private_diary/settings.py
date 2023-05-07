086961965f7032ad81376552b7d7796b2ddd03df
DESKTOP-SB45AQR\81802 <makun1010IT@gmail.com>
2023-05-07 15:08:24 +0900

    add region

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -1,6 +1,8 @@
 from .settings_common import *
 #本番運用環境用にセキュリティキーを生成し環境変数から読み込む
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+AWS_SES_REGION_NAME = 'us-east-1'
+AWS_SES_REGION_ENDPOINT = 'email.us-east-1.amazonaws.com'
 # デバッグモードを有効にするかどうか (本番運用では必ずFalseにする)
 DEBUG = False
 # 許可するホスト名のリスト
